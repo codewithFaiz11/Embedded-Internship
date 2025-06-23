db7a81317f7ad8c8ed3170926a0f857f6c283454
Mohd Faiz <mohdfaiz11034@gmail.com>
2025-06-24 03:59:42 +0530

    Add files via upload

---
+++ Signed Vs Unsigned/task3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "task3.h"
+
+int main() {
+    srand(time(NULL));
+    stData data;
+    
+    printf("1. Storing Negative Number\n");
+    data.svar = (rand() % 256) - 256;
+    data.uvar = (rand() % 256) - 256;
+    printf("svar stores the number %d whereas uvar can't, it overflows & stores %d\n\n", data.svar, data.uvar); 
+    
+    printf("2. Storing Number Greater Than 255\n");
+    data.svar = (rand() % 257) + 255;
+    data.uvar = (rand() % 257) + 255;
+    printf("uvar stores the number %d whereas svar can't, it overflows & stores %d\n\n", data.uvar, data.svar);  
+
+    printf("3. Storing Number in range 0 - 255\n");
+    data.svar = rand() % 256;
+    data.uvar = rand() % 256;
+    printf("Both svar and uvar can store the value %d", data.uvar); 
+
+    // CONCLUSION:
+    // 1. Signed variables (svar) can store negative numbers correctly but overflow when >255
+    // 2. Unsigned variables (uvar) can store large numbers (up to 511) but misinterpret negatives
+    // 3. Both work perfectly for 0-255 range
+
+    
+    return 0;
+}
---
+++ Signed Vs Unsigned/task3.h
@@ -0,0 +1,13 @@
+
+#ifndef _APP_H_
+#define _APP_H_
+
+
+typedef struct stData{
+    int svar:9;
+    unsigned int uvar:9;
+} stData;
+
+
+
+#endif // _APP_H_

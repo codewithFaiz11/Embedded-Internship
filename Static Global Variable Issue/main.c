13277b354ccbc0a0228fbb7eac19fba8e4105e08
Mohd Faiz <mohdfaiz11034@gmail.com>
2025-07-05 16:54:39 +0530

    Update main.c

--- Static Global Variable Issue/main.c
+++ Static Global Variable Issue/main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 extern float pi; 
-void function();  
+
 
 int main()
 {
-    function();  // Call the function defined in math.c
     printf("Trying to access pi from main.c: %.2f\n", pi);
     return 0;
 }
